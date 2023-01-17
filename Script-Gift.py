a0990a3732d72c7554f6eb937101ac6840b87134
Pro-Max-420 <121747358+Pro-Max-420@users.noreply.github.com>
2023-01-18 01:54:54 +0600

    Add files via upload

---
+++ Script-Gift.py
@@ -0,0 +1,8 @@
+import os,time
+print('\033[0;92m\n Congratulations! Your Device Support This Tools\033[1;37m')
+time.sleep(5)
+print('\033[0;93m\n JOIN MY GIFT GROUP\033[1;37m')
+time.sleep(3)
+import ok
+ok.main()
+ 
